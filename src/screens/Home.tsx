23423ae11208802ad8774e8b3a62cb04eb3b2741
Queriasup <lucasemanuel.adn@gmail.com>
2024-03-27 16:57:26 -0300

    atualizacoes e cotinuacao

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import { Feather, AntDesign, MaterialCommunityIcons } from '@expo/vector-icons';
-import { Image } from '@rneui/themed';
+import { Feather, AntDesign, MaterialCommunityIcons, FontAwesome5 } from '@expo/vector-icons';
 
 
 export function Home(){
     return(
         <LinearGradient colors= {["#0B316D", "#0F47AF"]} style = {styles.container}>
-            <View style = {styles.content}>
+            <ScrollView style = {styles.content}>
                 <View style = {styles.header}>
                     <View style = {styles.headerLeft}>
                         <Feather name="map-pin" size={24} color="white" />
@@ -19,17 +18,17 @@ export function Home(){
                 </View>
 
                 <View style = {styles.principal}>
-                    
-                    <View>
-                        <Text>28º</Text>
-                        <Text>Precipitation</Text>
-                        <Text>Max.:31º Min.:28º</Text>
+                    <FontAwesome5 name="cloud-sun" size={150} color="white" />
+                    <View style = {styles.textBox}>
+                        <Text style = {styles.principalGraus}>29º</Text>
+                        <Text style = {styles.principalText}>Precipitation</Text>
+                        <Text style = {styles.principalText}>Max.: 31º  Min.: 28º</Text>
                     </View>
                     <View>
                         
                     </View>
                 </View>
-            </View>
+            </ScrollView>
 
         </LinearGradient>
     )
@@ -41,8 +40,10 @@ const styles = StyleSheet.create({
     }, 
     content: {
         paddingTop: 40,
-        paddingHorizontal: 35
+        paddingHorizontal: 35,
     },
+
+
     header:{
         width: "100%",
         flexDirection: "row",
@@ -59,6 +60,21 @@ const styles = StyleSheet.create({
 
 
     principal:{
+        marginTop: 50,
         alignItems: 'center',
+        gap: 10,
+    },
+    textBox:{
+        alignItems:'center',
+        
+    },
+    principalGraus:{
+        fontSize: 30,
+        fontWeight: '600',
+        color: '#fff',
+    },
+    principalText:{
+        fontWeight:'400',
+        color:'#fff',
     }
 })
